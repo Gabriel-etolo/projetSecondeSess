baeba0d9bacb11b13062f9a446dfe6d24cc1ace7
Gabriel-etolo <64024242+Gabriel-etolo@users.noreply.github.com>
2020-08-08 13:42:39 +0200

    Update creation-procedure-ajIdBar.sql

--- backEnd/script-api-rest/creation-procedure-ajIdBar.sql
+++ backEnd/script-api-rest/creation-procedure-ajIdBar.sql
@@ -1,4 +1,4 @@
-create procedure ajIdBar(nomBar char(50))
+create procedure "DBA"."ajIdBar"(nomBar char(50))
 begin
 Call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
 insert into tbAvis(idBar)
