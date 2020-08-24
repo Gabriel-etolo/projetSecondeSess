f7baf509c0ccf01531fd0794608760b1324f1c9e
Gabriel-etolo <64024242+Gabriel-etolo@users.noreply.github.com>
2020-08-24 16:41:06 +0200

    Add files via upload

--- backEnd/script-serveur-web/creation-webservice.sql
+++ backEnd/script-serveur-web/creation-webservice.sql
@@ -1,58 +1,49 @@
-CREATE FUNCTION "DBA"."getPathCREATE PROCEDURE "DBA"."http_getCSS"(in url char(255))
-BEGIN
-call sa_set_http_header('Content-Type', 'text/css');
-Call sa_set_http_header('Access-Control-Allow-Origin', '*');
-select xp_read_file(dba.getPath() || 'CSS\' || url);
-END;
-COMMENT ON PROCEDURE "DBA"."http_getCSS" IS 'fournisseur de fichier .css (sous-répertoire CSS du site)';
-CREATE SERVICE "css" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getCSS(:url);
-COMMENT ON SERVICE "css" IS 'service fournisseur de css';
+CREATE FUNCTION "DBA"."getPath"()
 returns long varchar
 deterministic
 BEGIN
  declare dbPath long varchar;
- declare dbName long varchar;
- set dbPath = (select db_property ('file'));
- set dbName = (select db_property('name')) + '.db';
- set dbPath = left(dbPath, length(dbPath)-length(dbName));
- return dbPath;
- end;
- 
- 
- 
- CREATE PROCEDURE "DBA"."http_getPage"(in url char(255))
- BEGIN
- call sa_set_http_header('Content-Type', 'text/html; charset=utf-8');
- Call sa_set_http_header('Access-Control-Allow-Origin', '*');
- select xp_read_file(dba.getPath() || url || '.html');
- END;
-COMMENT ON PROCEDURE "DBA"."http_getPage" IS 'fournisseur de fichier .html (racine du site)';
+ declare dbName long varchar; 
+ set dbPath = (select db_property ('file'));       
+ set dbName = (select db_property('name')) + '.db'; 
+ set dbPath = left(dbPath, length(dbPath)-length(dbName)); 
+ return dbPath; // renvoyer path
+END;
 
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE PROCEDURE "DBA"."http_getPage"(in url char(255))
+
+BEGIN
+    call sa_set_http_header('Content-Type', 'text/html; charset=utf-8'); 
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
+	select xp_read_file(dba.getPath() || url || '.html');  
+END;
+CREATE SERVICE "page" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getPage(:url);
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE PROCEDURE "DBA"."http_getJS"(in url char(255))
 BEGIN
-call sa_set_http_header('Content-Type', 'application/javascript');
- Call sa_set_http_header('Access-Control-Allow-Origin', '*');
- select xp_read_file(dba.getPath() || 'js\' || url);
- END;
-COMMENT ON PROCEDURE "DBA"."http_getJS" IS 'fournisseur de fichier .js (sous répertoire JS du site)';
+  call sa_set_http_header('Content-Type', 'application/javascript'); // header http
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*'); // pas nécessaire si appels depuis le serveur - dangereux en production
+	select xp_read_file(dba.getPath() || 'js\' || url); 
+END;
+
 CREATE SERVICE "js" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getJS(:url);
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 CREATE PROCEDURE "DBA"."http_getCSS"(in url char(255))
 BEGIN
-call sa_set_http_header('Content-Type', 'text/css');
-Call sa_set_http_header('Access-Control-Allow-Origin', '*');
-select xp_read_file(dba.getPath() || 'CSS\' || url);
+  call sa_set_http_header('Content-Type', 'text/css'); 
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
+	select xp_read_file(dba.getPath() || 'CSS\' || url); 
 END;
-COMMENT ON PROCEDURE "DBA"."http_getCSS" IS 'fournisseur de fichier .css (sous-répertoire CSS du site)';
 CREATE SERVICE "css" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getCSS(:url);
-COMMENT ON SERVICE "css" IS 'service fournisseur de css';
-
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 CREATE PROCEDURE "DBA"."http_getIMG"(in url char(255))
 BEGIN
-call sa_set_http_header('Content-Type', 'image/png');
-Call sa_set_http_header('Access-Control-Allow-Origin', '*');
-select xp_read_file(dba.getPath() || 'IMG\' || url);
+  call sa_set_http_header('Content-Type', 'image/png'); 
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
+	select xp_read_file(dba.getPath() || 'IMG\' || url);
 END;
-COMMENT ON PROCEDURE "DBA"."http_getIMG" IS 'fournisseur de fichier graphique (sous-répertoire IMG du site)';
 CREATE SERVICE "img" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getIMG(:url);
